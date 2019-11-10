49bd4a2c0f71841c9bd00a4422361c03b259b912
Farooq Ahmed Khan - Personal Computer <farooqk.work@gmail.com>
2019-11-10 21:14:56 +0500

    Aligned the navbar with flato template . Added Menu text to be shown on mobile 7 tablets

--- src/components/AppNav/AppNav.js
+++ src/components/AppNav/AppNav.js
@@ -39,9 +39,10 @@ class AppNav extends Component {
             <header>
                 <nav id="sticky-navbar" class="navbar has-shadow has-text-centered">
                     <div class="navbar-brand">                
-                        <figure class="image is-64x64">
+                        {/* <figure class="image is-64x64">
                             <img src="profile_large.jpg" class="is-rounded" alt="FAK"  />
-                        </figure>
+                        </figure> */}
+                        <span class="menu-text is-hidden-fullhd is-hidden-widescreen is-hidden-desktop">MENU</span>
                         
                         <span class="navbar-burger burger icon is-large" onClick={() => this.toggleMobileNav()}><i class="fa fa-bars fa-2x"></i></span>
                         {/* {this.state.extend_burger_nav === true && (<span class="navbar-burger burger icon is-large" onClick={() => this.toggleMobileNav()}><i class="fa fa-close fa-3x"></i></span>)}                         */}
