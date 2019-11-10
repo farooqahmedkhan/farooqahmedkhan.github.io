af954b4a35f18d831bf154b4099ddfda6aa994f9
Farooq Ahmed Khan - Personal Computer <farooqk.work@gmail.com>
2019-11-10 17:16:01 +0500

    Altered the font size for hero title & subtitles for smaller, touch, desktop & full hd screens

--- src/components/AppHeroBanner/AppHeroBanner.js
+++ src/components/AppHeroBanner/AppHeroBanner.js
@@ -6,10 +6,10 @@ function AppHeroBanner(props) {
         <section class="hero is-fullheight has-background-info">
             <div class="hero-body">
                 <div class="container has-text-left">
-                    <h1 class="title has-text-white is-size-5 is-size-6-mobile is-size-5-tablet is-size-3-desktop">
+                    <h1 class="title has-text-white is-size-6-mobile is-size-4-tablet is-size-3-desktop is-size-2-widescreen is-size-1-fullhd">
                         {props.title}
                     </h1>
-                    <h2 class="subtitle has-text-white is-size-7 is-size-7-mobile is-size-5-desktop">
+                    <h2 class="subtitle has-text-white is-size-7-mobile is-size-6-tablet is-size-5-desktop is-size-4-widescreen is-size-3-fullhd">
                         {props.sub_title}
                     </h2>
                 </div>
