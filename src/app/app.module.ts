3db4ed7da31bd301a87d33fb963efbd72d1788ae
farooqahmedkhan <farooqahmedkhan>
2020-02-13 09:25:58 +0500

    Updated structure  - Include micro css framework Chota  - Angular 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent    
+    NavigationComponent,
+    HeaderComponent    
   ],
   imports: [
     BrowserModule,
